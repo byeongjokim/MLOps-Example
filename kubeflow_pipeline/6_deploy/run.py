48dbe8beca1ad9eb3bdae0ad426489e0d7759c7b
byeongjokim <byeongjooooo.kim@gmail.com>
2021-01-08 23:09:32 +0900

    yaml to sdk

--- kubeflow_pipeline/6_deploy/run.py
+++ kubeflow_pipeline/6_deploy/run.py
@@ -38,22 +38,127 @@ def archive(args, version):
     os.system(cmd)
 
 def serving(args, version):
-    with open("service.yaml") as f:
-        svc_yaml = yaml.safe_load(f)
+    # with open("service.yaml") as f:
+    #     svc_yaml = yaml.safe_load(f)
 
-    svc_yaml["metadata"]["labels"]["app.kubernetes.io/version"] = version
+    # svc_yaml["metadata"]["labels"]["app.kubernetes.io/version"] = version
 
-    with open("deployment.yaml") as f:
-        dep_yaml = yaml.safe_load(f)
+    # with open("deployment.yaml") as f:
+    #     dep_yaml = yaml.safe_load(f)
     
-    dep_yaml["metadata"]["labels"]["app.kubernetes.io/version"] = version
-    
+    # dep_yaml["metadata"]["labels"]["app.kubernetes.io/version"] = version
     config.load_incluster_config()
+
     k8s_apps_v1 = client.AppsV1Api()
-    
-    k8s_apps_v1.create_namespaced_deployment(body=dep_yaml, namespace="kbj")
+    template = client.V1PodTemplateSpec(
+        metadata=client.V1ObjectMeta(
+            labels={"app":"torchserve"}
+        ),
+        spec=client.V1PodSpec(
+            containers=[
+                volumes=[
+                    client.V1Volume(
+                        name="persistent-storage",
+                        persistent_volume_claim=client.V1PersistentVolumeClaimVolumeSource(
+                            claim_name="serving-model-pvc"
+                        )
+                    )
+                ],
+                container = client.V1Container(
+                    name="torchserve",
+                    image="byeongjokim/torchserve",
+                    args=["torchserve", "--start",  "--model-store", "/home/model-server/shared/model-store/", "--ts-config", "/home/model-server/shared/config/config.properties"],
+                    image_pull_policy="Always",
+                    ports=[
+                        client.V1ContainerPort(
+                            name="ts",
+                            container_port=8082
+                        ),
+                        client.V1ContainerPort(
+                            name="ts-management",
+                            container_port=8083
+                        ),
+                        client.V1ContainerPort(
+                            name="ts-metrics",
+                            container_port=8084
+                        )
+                    ],
+                    volume_mounts=[
+                        client.V1VolumeMount(
+                            name="persistent-storage",
+                            mountPath="/home/model-server/shared/"
+                        )
+                    ]
+                    resources=client.V1ResourceRequirements(
+                        limits={
+                            "cpu":1,
+                            "memory":"4Gi",
+                            "nvidia.com/gpu": 0
+                        },
+                        requests={
+                            "cpu":1,
+                            "memory":"1Gi",
+                        }
+                    )
+                )
+            ]
+
+        )
+    )
+    deployment = client.V1Deployment(
+        api_version="apps/v1",
+        kind="Deployment",
+        metadata=client.V1ObjectMeta(
+            name="torchserve",
+            labels={
+                "app":"torchserve",
+                "app.kubernetes.io/version":version
+            }
+        ),
+        spec=client.V1DeploymentSpec(
+            replicas=1,
+            selector=client.V1LabelSelector(
+                match_labels={"app":"torchserve"}
+            )
+            template=template
+        )
+    )
+    k8s_apps_v1.create_namespaced_deployment(body=deployment, namespace="kbj")
     print("[+] Deployment created")
 
+    k8s_core_v1 = client.CoreV1Api()
+    body = client.V1Service(
+        api_version="v1",
+        kind="Service",
+        metadata=client.V1ObjectMeta(
+            name="service-example",
+            labels={
+                "app":"torchserve",
+                "app.kubernetes.io/version":version
+            }
+        ),
+        spec=client.V1ServiceSpec(
+            type="LoadBalancer",
+            selector={"app":"torchserve"},
+            ports=[
+                client.V1ServicePort(
+                    name="preds",
+                    port=8082,
+                    target_port="ts"
+                ),
+                client.V1ServicePort(
+                    name="mdl",
+                    port=8083,
+                    target_port="ts-management"
+                ),
+                client.V1ServicePort(
+                    name="metrics",
+                    port=8084,
+                    target_port="ts-metrics"
+                )
+            ]
+        )
+    )
     k8s_apps_v1.create_namespaced_service(body=svc_yaml, namespace="kbj")
     print("[+] Service created")
     
